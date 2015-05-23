87c8a6c4b3229ef256263aecbf7664a9156d12c2
neville-lewis <nlewis@infinite-usa.com>
2015-05-23 09:11:16 +0530

    First commit

---
+++ MiddlewareClass.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+namespace OwinMiddleWareConsoleApp
+{
+
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    public class StronglyTypedMiddlewareClass : OwinMiddleware
+    {
+        public StronglyTypedMiddlewareClass(OwinMiddleware next) : base (next)
+        {
+            Console.WriteLine("StronglyTypedMiddlewareClass instantiated.");
+        }
+
+        public async override Task Invoke(IOwinContext context)
+        {
+            Console.WriteLine("StronglyTypedMiddlewareClass was invoked");
+            await Next.Invoke(context);
+        }
+    }
+
+
+
+    public class MiddlewareClass
+    {
+        private AppFunc _next;
+        private MiddleWareDependency _dependency;
+
+
+ 
+
+        public MiddlewareClass (AppFunc next, MiddleWareDependency dependency)
+        {
+            Console.WriteLine("MiddlewareClass instantiated.");
+            _next = next;
+            _dependency = dependency;
+        }
+
+        public async Task Invoke(IDictionary<string , object> environment)
+        {
+            Console.WriteLine("Middleware class invoked.");
+            await _next.Invoke(environment);
+        }
+    }
+
+
+    public class AnotherMiddlewareClass
+    {
+        private AppFunc _next;
+        private MiddleWareDependency _dependency;
+
+
+
+        public void Initialize(AppFunc next)
+        {
+            this._next = next;
+        }
+
+
+
+        public AnotherMiddlewareClass(MiddleWareDependency dependency)
+        {
+            Console.WriteLine("MiddlewareClass instantiated.");
+            
+            _dependency = dependency;
+        }
+
+        public async Task Invoke(IDictionary<string, object> environment)
+        {
+            Console.WriteLine("Middleware class invoked.");
+            await _next.Invoke(environment);
+        }
+    }
+
+    public class MiddleWareDependency
+    {
+
+        public MiddleWareDependency()
+        {
+            Console.WriteLine("MiddleWareDependency Constructor called");
+        }
+
+        public void DependencyMethod()
+        {
+            Console.WriteLine("MiddleWareDependency method was called");
+        }
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(OwinMiddleWareConsoleApp.Startup))]
+
+namespace OwinMiddleWareConsoleApp
+{
+    /// <summary>
+    ///  Source: http://benfoster.io/blog/how-to-write-owin-middleware-in-5-different-steps
+    /// </summary>
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+
+            app.Use(typeof(StronglyTypedMiddlewareClass));
+            app.Use(typeof(MiddlewareClass), new MiddleWareDependency());
+
+            //the above line for dependency injection can also be done like this below - 
+            MiddleWareDependency dep = new MiddleWareDependency();
+            AnotherMiddlewareClass amw = new AnotherMiddlewareClass(dep);
+            app.Use(amw);
+
+            //MiddlewareClass mw = new MiddlewareClass(null, dep);// this doesnt work: Check this link for answers: http://stackoverflow.com/questions/30152602/passing-in-a-parameter-to-appfunc
+            //app.Use(mw);
+
+        }
+    }
+}
